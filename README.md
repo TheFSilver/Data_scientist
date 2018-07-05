f0c125a9a88aea40d20f81ca338339b62ef34922
TheFSilver <dasylva.f@gmail.com>
2018-07-05 19:09:00 +0200

    Ajout des exos

---
+++ README.md
@@ -0,0 +1,3 @@
+Repository des exercices journalist.rb et crypto.rb pour THP par Jeremy Rak (
+https://github.com/skageraz/Data_scientist ) et Francois D.
+(https://github.com/TheFSilver/Data_scientist)
---
+++ journalist.rb
@@ -0,0 +1,89 @@
+# Creation de l'array journalist
+journalist = Array.new
+journalist = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+puts "Combien y a t'il de journalistes dans cet array ?"
+# compte et affiche le nombre d'items dans l'array
+puts journalist.size
+
+puts "Combien d'handle contiennent un numéro ?"
+# puts journalist.select { |i| i =~ /\d/}.count
+# code simplifié via les regexp
+
+# Loop conditionnelle verifiant dans chaque item de l'array la présence d'un chiffre et le convertit en string. Si correct, la variable nombre sera incrémenté.
+nombre = 0
+journalist.each do |i|
+  for x in 0..9
+    if i.include?(x.to_s) == true
+      nombre+=1
+      break
+    end
+  end
+end
+
+# affiche nombre. nombre étant le nombre d'handles contenant un numéro
+puts nombre
+
+puts "Combien de Aude ?"
+# vérifie et compte les occurences de "aude" pour chaque item de l'array
+puts journalist.select { |i| i.include?('aude')}.count
+
+puts "Combien commencent par une majuscule ?"
+# puts journalist.select { |i| ( i[1] =~ /[A-Z]/ )}.count
+# code simplifié via les regexp
+
+# Loop conditionnelle verifiant la présence d'une majuscule sur la première lettre de chaque item de l'array. Si correct, la variable first_maj sera incrémenté.
+first_maj = 0
+journalist.each do |i|
+  for x in ('A'..'Z')
+    if i[1].include?(x) == true
+      first_maj+=1
+      break
+    end
+  end
+end
+
+# affiche first_maj. first_maj étant le nombre d'item commençant par une majuscule
+puts first_maj
+
+puts "Combien contiennent une majuscule ?"
+# puts journalist.select { |i| ( i =~ /[A-Z]/ )}.count
+# code simplifié via les regexp
+
+# Loop conditionnelle verifiant la présence d'une majuscule sur dans chaque item de l'array. Si correct, la variable maj sera incrémenté.
+maj = 0
+journalist.each do |i|
+  for x in ('A'..'Z')
+    if i.include?(x) == true
+      maj+=1
+      break
+    end
+  end
+end
+
+# affiche maj. maj étant le nombre d'item contenant une majuscule
+puts maj
+
+puts "Combien de _ dans tous les pseudos confondus ?"
+#puts journalist.find_all { |i| i.include?("_")}.count
+
+# Loop conditionnelle verifiant et comptant le nombre de "_" dans la totalité de l'array. Si correct, la variable under_ sera incrémenté.
+under_ = 0
+journalist.each do |i|
+  j = i.length
+  for x in (0..j)
+    if i[x] == '_'
+      under_ += 1
+    end
+  end
+end
+
+# affiche under_. under_ étant le nombre de fois que "_" apparait 
+puts under_
+
+
+puts "Trie la liste de handle par ordre alphabétique"
+# tri l'array par ordre alphabétique majuscules et minuscules confondus
+puts journalist.sort_by { |x| x.downcase }
+
+
